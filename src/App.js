a10d82679e43b72efcffd4e3068061c9feb7c9c9
amondi614 <amondiocholla12@gmail.com>
2022-02-10 16:20:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Rreact from "react";
+
+http://api.icndb.com/jokes/random
+
+function App() {
+  return (
+    <div class="box">
+      <h1>Chuck Norris Jokes Generator</h1>
+      <p>Here goes the joke</p>
+      <button>Get new joke!</button>
+      Hello friend!
+    </div>
+  );
+}
+
+export default App;
